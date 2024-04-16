bd16801d2a64f71a9ccf351bcac4cdf853a9cd8a
sundarshahi <shahithakurisundar@gmail.com>
2024-04-16 16:37:32 +0545

    chore: setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-lg shadow-gray-200 from-yellow-500 to-orange-500`}
+    >
+      Weather App
+    </div>
+  );
+}
+
+export default App;
